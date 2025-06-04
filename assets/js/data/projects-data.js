644be0d015ea3c4275ce60c498f18d9e62f8f03e
GHPXD <ghp17@outlook.com>
2025-06-04 18:04:07 -0300

    Add Projetos Extrator NF e Pycovid

--- assets/js/data/projects-data.js
+++ assets/js/data/projects-data.js
@@ -8,12 +8,22 @@ const projectsData = {
         category: 'Data'
     },
     'Insight de Casos de Covid-19': {
-        repo: 'https://github.com/GHPXD/PJBL-01',
-        readme: './data/docs/Run Man Jump.md',
-        tech: ['python', 'pandas', 'pyplot'],
+        repo: 'https://github.com/GHPXD/Pycovid',
+        readme: './data/docs/Pycovid.md',
+        tech: ['python', 'pandas', 'numpy', 'matplotlib', 'seaborn', 'plotly', 'dash', 'Tkinter'], 
         image: './assets/media/projects/PyCovid.png',
         category: 'Data'
     },
+    'Extrator Nota Fiscal': {
+        repo: 'https://github.com/GHPXD/pdf-extractor',
+        readme: './data/docs/Extrator Nota Fiscal.md',
+        tech: ['python', 'pandas', 'numpy','seaborn', 'matplotlib', 'pdf2image', 'tkinter', 'selenium', 'tensorflow', 'pytesseract'],
+        image: './assets/media/projects/',
+        category: 'Data'
+    },
+
+    
+
     'Jump Man Game': {
         repo: 'https://ghpxd.github.io/run-man/',
         readme: './assets/docs/Run Man Jump.md',
@@ -28,11 +38,15 @@ const projectsData = {
         image: './assets/media/projects/Jokenpo.png',
         category: 'Web'
     },
+
+
+
     'Sistema Pet Shop': {
         repo: 'https://github.com/GHPXD/PJBL_POO_01',
         readme: './data/docs/projeto5.md',
         tech: ['java'],
         image: './assets/media/projects/petshop.jpg',
         category: 'Backend'
-    }
+    },
+    
 };
