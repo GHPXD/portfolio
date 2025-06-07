e120d6d4917a0fb22ee53a8f49ec1e95ac7ac88f
GHPXD <ghp17@outlook.com>
2025-06-07 00:51:09 -0300

    Correção diretorio imagens e readme

--- assets/js/data/projects-data.js
+++ assets/js/data/projects-data.js
@@ -4,23 +4,23 @@
 const projectsData = {
     'Análise de Reviews Negativos com PySpark': {
         repo: 'https://github.com/GHPXD/Comparison-of-Negative-Movie-Reviews',
-        readme: './data/docs/Comparison of Negative Movie Reviews.md',
+        readme: './assets/docs/Comparison of Negative Movie Reviews.md',
         tech: ['python', 'pandas', 'pyplot', 'pyspark'],
         image: './assets/media/projects/Analise_Reviews_PySpark.png',
         category: 'Data'
     },
     'Insight de Casos de Covid-19': {
         repo: 'https://github.com/GHPXD/Pycovid',
-        readme: './data/docs/Pycovid.md',
+        readme: './assets/docs/Pycovid.md',
         tech: ['python', 'pandas', 'numpy', 'matplotlib', 'seaborn', 'plotly', 'dash', 'tkinter'], 
         image: './assets/media/projects/PyCovid.png',
         category: 'Data'
     },
     'Extrator Nota Fiscal': {
         repo: 'https://github.com/GHPXD/pdf-extractor',
-        readme: './data/docs/Extrator Nota Fiscal.md',
+        readme: './assets/docs/Extrator Nota Fiscal.md',
         tech: ['python', 'pandas', 'numpy','seaborn', 'matplotlib', 'pdf2image', 'tkinter', 'selenium', 'tensorflow', 'pytesseract'],
-        image: './assets/media/projects/',
+        image: './assets/media/projects/Extrator Nota Fiscal.png',
         category: 'Data'
     },
 
@@ -29,9 +29,9 @@ const projectsData = {
 // AUTOMAÇÃO
     'Automação de Aprovação de Tarefas - Sistema CAP': {
         repo: 'https://github.com/GHPXD/CAP',
-        readme: './data/docs/CAP.md',
+        readme: './assets/docs/CAP.md',
         tech: ['python', 'pandas', 'selenium', 'tkinter'],
-        image: './assets/media/projects/',
+        image: './assets/media/projects/CAP.png',
         category: 'Automacao'
     },
     
@@ -47,7 +47,7 @@ const projectsData = {
     },
     'Jokenpo Game': {
         repo: 'https://ghpxd.github.io/Jokenpo/',
-        readme: './data/docs/Jokenpo.md',
+        readme: './assets/docs/Jokenpo.md',
         tech: ['html', 'css', 'javascript'],
         image: './assets/media/projects/Jokenpo.png',
         category: 'Web'
