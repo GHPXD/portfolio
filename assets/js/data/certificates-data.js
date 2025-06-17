2a76c4fc2b817cdc7a2461cbfd4e151cfc041259
GHPXD <ghp17@outlook.com>
2025-06-17 08:59:12 -0300

    Add certificado DIO Expert

--- assets/js/data/certificates-data.js
+++ assets/js/data/certificates-data.js
@@ -1,20 +1,23 @@
 // Dados dos certificados
 const certificatesData = {
-    'Certificado Yellow Belt': {
-        image: [
-            './assets/media/certificates/Yellow Belt01.jpg',
-            './assets/media/certificates/Yellow Belt02.jpg'
-        ],
-        category: 'diversos'
-    },
+
+    // Fullstack
     'Santander Bootcamp 2023 - Fullstack Java + Angular': {
         image: ['./assets/media/certificates/JAVA-ANGULAR.jpg'],
         category: 'full'
     },
-    'GIT - Básico ao avançado': {
-        image: ['./assets/media/certificates/Git.jpg'],
-        category: 'diversos'
-    },
+
+
+
+    // Backend
+
+
+
+    // Frontend
+
+
+
+    // Dados
     'Programa Desenvolve Boticario': {
         image: [
             './assets/media/certificates/Programa_Desenvolve01.png', 
@@ -22,8 +25,30 @@ const certificatesData = {
         ],
         category: 'dados'
     },
+
     'Santander Bootcamp 2023 - Ciencia de Dados com Python': {
         image: ['./assets/media/certificates/Ciencia de Dados com Python.png'],
         category: 'dados'
+    },
+
+
+
+    // Diversos
+    'Certificado Yellow Belt': {
+        image: [
+            './assets/media/certificates/Yellow Belt01.jpg',
+            './assets/media/certificates/Yellow Belt02.jpg'
+        ],
+        category: 'diversos'
+    },
+
+    'GIT - Básico ao avançado': {
+        image: ['./assets/media/certificates/Git.jpg'],
+        category: 'diversos'
+    },
+    
+    'Campus DIO Expert': {
+        image: ['./assets/media/certificates/Dio Campus Expert.png'],
+        category: 'diversos'
     }
 };
